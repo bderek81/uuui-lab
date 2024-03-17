e1d0569ff691040aa60b62c0156ec23cb593b76e
bderek81 <bozo.derek@fer.hr>
2024-03-17 14:13:07 +0100

    argparse + uuui_zip => grade

--- lab1py/solution.py
+++ lab1py/solution.py
@@ -1,1 +1,18 @@
-print()
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--alg")
+    parser.add_argument("--ss")
+    parser.add_argument("--h")
+    parser.add_argument("--check-optimistic")
+    parser.add_argument("--check-consistent")
+    
+    return parser.parse_args()
+
+def main():
+    args = parse_arguments()
+
+if __name__ == "__main__":
+    main()
--- uuui_zip.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set JMBAG=0036542918
-
-set lab=1
-mkdir autograder\solutions\%JMBAG%
-zip autograder\solutions\%JMBAG%\%JMBAG%.zip lab%lab%py\*
