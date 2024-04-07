978d5f56e0a2cdaae60971e24b968306965432bf
bderek81 <bozo.derek@fer.hr>
2024-04-07 15:33:49 +0200

    simplify print_resolution_results

--- lab2py/solution.py
+++ lab2py/solution.py
@@ -4,11 +4,11 @@ from collections import deque
 class Clause:
     sep = " v "
 
-    def __init__(self, raw_clause: str, sos=False, parents: 'tuple[Clause]' = None):
+    def __init__(self, raw_clause: str, sos=False, parents: 'tuple[Clause]'=None):
         self.literals = frozenset(raw_clause.split(Clause.sep))
         self.sos = sos
-        self.nil = not raw_clause
         self.parents = parents
+        self.nil = not raw_clause
     
     def __repr__(self):
         return "NIL" if self.nil else Clause.sep.join(self.literals)
@@ -20,30 +20,19 @@ class Clause:
         return hash(self.literals)
     
     def negation(self):
-        return {Clause(negate(literal), True) for literal in self.literals}
+        return {Clause(negate(lit), sos=True) for lit in self.literals}
     
     def is_tautology(self):
-        for literal in self.literals:
-            if negate(literal) in self.literals:
-                return True
+        return any(negate(lit) in self.literals for lit in self.literals)
 
-        return False
-
-def negate(literal: str):
-    return f"~{literal}" if literal[0] != '~' else literal[1:]
+def negate(lit: str):
+    return f"~{lit}" if lit[0] != '~' else lit[1:]
 
 def remove_redundant(clauses: 'set[Clause]'):
-    for c1 in clauses:
-        redundant = False
-        for c2 in clauses:
-            if c2.literals < c1.literals:
-                redundant = True
-                break
-        if redundant: continue
-        yield c1
+    return {c for c in clauses if not any(c0.literals < c.literals for c0 in clauses)}
 
 def remove_irrelevant(clauses: 'set[Clause]'):
-    return set(filter(lambda clause: not clause.is_tautology(), clauses))
+    return {c for c in clauses if not c.is_tautology()}
 
 def select_clauses(clauses: 'set[Clause]'):
     visited = set()
@@ -52,26 +41,26 @@ def select_clauses(clauses: 'set[Clause]'):
         for c2 in filter(lambda c: c not in visited, clauses):
             if not (c1.sos or c2.sos): continue
 
-            for literal in c1.literals:
-                if negate(literal) in c2.literals:
+            for lit in c1.literals:
+                if negate(lit) in c2.literals:
                     yield (c1, c2)
                     break
 
 def resolve(c1: Clause, c2: Clause):
-    c1_res_literals = set(c1.literals)
+    c1_res_lits = set(c1.literals)
 
     res_by = None
     for lit in c1.literals:
         if negate(lit) in c2.literals:
             if res_by is not None: return None
             res_by = lit
-            c1_res_literals.remove(res_by)
+            c1_res_lits.remove(res_by)
     
-    c2_res_literals = set(c2.literals)
-    c2_res_literals.remove(negate(res_by))
+    c2_res_lits = set(c2.literals)
+    c2_res_lits.remove(negate(res_by))
     
-    raw_clause = Clause.sep.join(c1_res_literals.union(c2_res_literals))
-    return Clause(raw_clause, True, (c1, c2))
+    raw_clause = Clause.sep.join(c1_res_lits.union(c2_res_lits))
+    return Clause(raw_clause, sos=True, parents=(c1, c2))
 
 def print_dashed_ln(len=15): print('=' * len)
 
@@ -80,50 +69,44 @@ def print_resolution_result(
     goal: Clause,
     resolvent: Clause,
 ):
-    def indexed_clause(index: int, clause: Clause):
-        indexed_clause = f"{index}. {clause}"
-        if clause.parents:
-            parent_index = sorted(map(lambda p: clause_index[p], clause.parents))
-            indexed_clause += f" ({parent_index[0]}, {parent_index[1]})"
-        return indexed_clause
+    def print_indexed(clauses: 'deque[Clause]'):
+        for c in clauses:
+            indexed_c = f"{clause_index[c]}. {c}"
+            if c.parents:
+                parent_index = sorted(clause_index[p] for p in c.parents)
+                indexed_c += f" ({parent_index[0]}, {parent_index[1]})"
+            print(indexed_c)
     
     conclusion = resolvent is not None
     if not conclusion:
-        for index, clause in enumerate(input_clauses, start=1):
-            print(indexed_clause(index, clause))
+        clause_index = {c: i for i, c in enumerate(input_clauses, 1)}
     else:
-        input_clauses = deque()
-        goal_negation, goal_negation_clauses = goal.negation(), deque()
-        other_clauses = deque()
+        input_clauses, derived_clauses = deque(), deque()
 
         queue = deque([resolvent])
         while queue:
             clause = queue.popleft()
             if clause.parents:
-                if not clause.nil: other_clauses.appendleft(clause)
+                derived_clauses.appendleft(clause)
                 queue.extend(clause.parents)
-            elif clause in goal_negation:
-                goal_negation_clauses.appendleft(clause)
+            elif clause in goal.negation():
+                input_clauses.append(clause)
             else:
                 input_clauses.appendleft(clause)
         
         input_clauses = deque(dict.fromkeys(input_clauses))
-        goal_negation_clauses = deque(dict.fromkeys(goal_negation_clauses))
-        other_clauses = deque(dict.fromkeys(other_clauses))
-        
-        input_clauses.extend(goal_negation_clauses)
-        clause_index = {clause: i for i, clause in enumerate(input_clauses, 1)}
-        next_i = len(clause_index) + 1
-        clause_index.update({clause: i for i, clause in enumerate(other_clauses, next_i)})
-        next_i = len(clause_index) + 1
+        clause_index = {c: i for i, c in enumerate(input_clauses, 1)}
 
-        for clause in input_clauses:
-            print(indexed_clause(clause_index[clause], clause))
-        print_dashed_ln()
-        for clause in other_clauses:
-            print(indexed_clause(clause_index[clause], clause))
-        print(indexed_clause(next_i, resolvent))
+        derived_clauses = deque(dict.fromkeys(derived_clauses))
+        clause_index.update(
+            {c: i for i, c in enumerate(derived_clauses, len(clause_index) + 1)}
+        )
+
+    print_indexed(input_clauses)
     print_dashed_ln()
+    if conclusion:
+        print_indexed(derived_clauses)
+        print_dashed_ln()
     print(f"[CONCLUSION]: {goal} is {'true' if conclusion else 'unknown'}")
 
 def resolution(clauses: 'set[Clause]', goal: Clause):
@@ -133,7 +116,7 @@ def resolution(clauses: 'set[Clause]', goal: Clause):
     input_clauses.extend(goal_negation)
     clauses.update(goal_negation)
 
-    clauses = set(remove_redundant(remove_irrelevant(clauses)))
+    clauses = remove_redundant(remove_irrelevant(clauses))
 
     new = set()
     while True:
@@ -194,7 +177,7 @@ def main():
     clauses, goal = input_clauses(lines(args.clauses))
 
     if args.task == "resolution":
-        # print(goal)
+        print(goal)
         print_resolution_result(*resolution(clauses, goal))
     elif args.task == "cooking":
         user_cmds = input_user_cmds(lines(args.user_cmds))
