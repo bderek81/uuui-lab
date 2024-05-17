1bc6da5c4777ec8bcf1314e5f5a6c1d55ecc281c
bderek81 <bozo.derek@fer.hr>
2024-05-17 20:02:56 +0200

    improve readability

--- lab3py/solution.py
+++ lab3py/solution.py
@@ -15,7 +15,10 @@ class ID3:
     def __init__(self, id3_depth: int):
         self.depth = id3_depth
     
-    def id3(self, D: list, D_parent: list, X: 'list[str]', y: str, depth=1):
+    def id3(self, 
+        D: 'list[dict]', D_parent: 'list[dict]',
+        X: 'list[str]', y: str, depth=1
+    ):
         if depth > self.depth:
             v = most_common(row[y] for row in D)
             return Leaf(v)
@@ -25,23 +28,23 @@ class ID3:
             return Leaf(v)
         
         v = most_common(row[y] for row in D)
-        if not X or all(map(lambda row: row[y]==v, D)):
+        if not X or all(map(lambda row: row[y] == v, D)):
             return Leaf(v)
         
-        ig_list = []
-        x, max = str(), -inf
-        for x_i in X:
-            ig_x = information_gain(D, x_i, y)
-            ig_list.append((x_i, ig_x))
-            if ig_x > max:
-                x, max = x_i, ig_x
-        print(' '.join(map(lambda z: f"IG({z[0]})={z[1]:.4f}", sorted(ig_list, key=lambda w: w[1], reverse=True))))
+        IG_Dx = sorted(
+            map(lambda x: (IG(D, x, y), x), X),
+            key=lambda ig_x: (-ig_x[0], ig_x[1])
+        )
+        print_information_gain(IG_Dx)
+        x = IG_Dx[0][1]
 
         node = Node(x)
-        V_x = Counter(row[x] for row in D).most_common()
-        for v, _ in V_x:
-            t = self.id3(list(filter(lambda row: row[x]==v, D)), D, list(filter(lambda z: z!=x, X)), y, depth+1)
-            node.subtrees.append((v, t)) if t is not None else None
+        V_x = {row[x] for row in D}
+        for v in V_x:
+            D_xv = list(filter(lambda row: row[x] == v, D))
+            X_x = list(filter(lambda x_i: x_i != x, X))
+            t = self.id3(D_xv, D, X_x, y, depth+1)
+            node.subtrees.append((v, t))
         return node
 
     def fit(self, train_dataset: str):
@@ -49,29 +52,17 @@ class ID3:
         fields = list(D[0].keys())
         X, y = sorted(fields[:-1]), fields[-1]
 
-        self.decision_tree = self.id3(D, D, X, y)
+        self.tree = self.id3(D, D, X, y)
         print("[BRANCHES]:")
-        ID3.print_branches(self.decision_tree)
+        print_branches(self.tree)
     
     @staticmethod
-    def print_branches(decision_tree: 'Leaf | Node', string: str="", level=1):
-        if type(decision_tree) == Leaf:
-            print(f"{string} {decision_tree.value}")
-            return
-        
-        for v, t in decision_tree.subtrees:
-            string_cpy = string
-            string += f"{' ' if level>1 else ''}{level}:{decision_tree.feature}={v}"
-            ID3.print_branches(t, string, level+1)
-            string = string_cpy
-    
-    @staticmethod
-    def decide(decision_tree: 'Leaf | Node', row: dict):
-        if type(decision_tree) == Leaf:
-            return decision_tree.value
+    def decide(tree: 'Leaf | Node', row: dict):
+        if type(tree) == Leaf:
+            return tree.value
 
-        for v, t in decision_tree.subtrees:
-            if row[decision_tree.feature]==v:
+        for v, t in tree.subtrees:
+            if row[tree.feature] == v:
                 return ID3.decide(t, row)
 
     def predict(self, test_dataset: str):
@@ -80,18 +71,19 @@ class ID3:
         
         labels = sorted({row[y] for row in D})
         confusion_matrix = [[0 for _ in labels] for _ in labels]
-        index_dict = {x: i for i, x in enumerate(labels)}
+        label_index = {l_i: i for i, l_i in enumerate(labels)}
         
-        correct, total = 0, len(D)
         predictions = "[PREDICTIONS]:"
+        correct, total = 0, len(D)
         for row in D:
-            decision = ID3.decide(self.decision_tree, row)
+            decision = ID3.decide(self.tree, row)
             if decision is None:
                 decision = most_common(row[y] for row in D)
             
             predictions += f" {decision}"
-            correct += decision==row[y]
-            confusion_matrix[index_dict[row[y]]][index_dict[decision]] += 1
+            correct += decision == row[y]
+            confusion_matrix[label_index[row[y]]][label_index[decision]] += 1
+        
         print(predictions)
         print(f"[ACCURACY]: {correct / total:.5f}")
         print_confusion_matrix(confusion_matrix)
@@ -100,26 +92,40 @@ def most_common(iterable):
     e_cnt = Counter(iterable).most_common()
     return sorted(filter(lambda z: z[1] == e_cnt[0][1], e_cnt))[0][0]
 
-def entropy(D: list, y: str):
-    K = Counter(row[y] for row in D).most_common()
+def entropy(D: 'list[dict]', y: str):
+    pmf_y = map(
+        lambda cnt: cnt / len(D),
+        Counter(row[y] for row in D).values()
+    )
 
-    return -sum(i/len(D) * log2(i/len(D)) for _, i in K)
+    return -sum(P_yi * log2(P_yi) for P_yi in pmf_y)
 
-def information_gain(D: list, x: str, y: str):
-    V_x = Counter(row[x] for row in D).most_common()
+def IG(D: 'list[dict]', x: str, y: str):
     sum = 0
-    for v, _ in V_x:
-        D_xv = list(filter(lambda row: row[x]==v, D))
+    V_x = {row[x] for row in D}
+    for v in V_x:
+        D_xv = list(filter(lambda row: row[x] == v, D))
         sum += len(D_xv) * entropy(D_xv, y)
     
     return entropy(D, y) - sum / len(D)
+
+def print_information_gain(IG_Dx: 'list[tuple[float, str]]'):
+    print(' '.join(map(lambda ig_x: f"IG({ig_x[1]})={ig_x[0]:.4f}", IG_Dx)))
+
+def print_branches(tree: 'Leaf | Node', string="", level=1):
+    if type(tree) == Leaf:
+        print(f"{string} {tree.value}")
+        return
+    
+    for v, t in tree.subtrees:
+        print_branches(t, ' '.join((string, f"{level}:{tree.feature}={v}")), level+1)
 
 def print_confusion_matrix(confusion_matrix: 'list[list[int]]'):
     print("[CONFUSION_MATRIX]:")
     for row in confusion_matrix:
         print(*row)
 
-def input_csv(file: str):
+def input_csv(file: str) -> 'list[dict]':
     return [row for row in csv.DictReader(open(file))]
 
 def parse_arguments():
